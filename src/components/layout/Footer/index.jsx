eb9b50ed149319c8e04d998f679af7d2b2ccb602
lucasamaraloliveira <lucas.amaral.oliveira.silva@gmail.com>
2022-12-07 00:00:32 -0300

    fix: page responsiveness fix

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div className='footer-color'>
-      <div className='social-media'>
-      <h3 id='Contact'>Contact</h3> 
-        <ul >
-          <li><a href="https://github.com/lucasamaraloliveira" target='_blank'> Github</a></li>
-          <li><a href=""> LinkedIn</a></li>
+    <div className="footer-color">
+      <div className="social-media">
+        <h3 id="Contact">Contact</h3>
+        <ul>
+          <li>
+            <a href="https://github.com/lucasamaraloliveira" target="_blank">
+              {" "}
+              Github
+            </a>
+          </li>
+          <li>
+            <a href="">LinkedIn</a>
+          </li>
         </ul>
       </div>
-      <footer>Desenvolvido por<b> Lucas Amaral&copy;</b></footer>
+      <footer>
+        <p> Desenvolvido por Lucas Amaral</p>
+      </footer>
     </div>
-    
-    
-  )
+  );
 }
 
-export default Footer
+export default Footer;
